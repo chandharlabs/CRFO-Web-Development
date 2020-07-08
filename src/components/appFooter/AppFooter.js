13eb1ad59d649c115fe6a29af048a3f7c3c976f6
Chandhar Research Labs <48096296+chandharlabs@users.noreply.github.com>
2020-07-08 05:55:02 +0530

    Update AppFooter.js

--- src/components/appFooter/AppFooter.js
+++ src/components/appFooter/AppFooter.js
@@ -6,15 +6,7 @@ const AppFooter = props => {
   return (
     <div className={cx("footer-container")}>
       <footer>
-        Updated Live with data from{" "}
-        <a
-          href="https://www.mohfw.gov.in/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ministry of Health and Family Welfare
-        </a>
-        , India Mapped using{" "}
+        India Mapped using{" "}
         <a
           href="https://openstreetmap.org/"
           target="_blank"
