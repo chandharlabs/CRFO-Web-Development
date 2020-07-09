dccde4b6f61e6091b5884ac88d4bc917192f2576
adgai19 <gaitondeaditya@gmail.com>
2020-07-09 20:48:23 +0530

    statecenterindex function done .

--- src/components/statelist.jsx
+++ src/components/statelist.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
 import Collapse from '@material-ui/core/Collapse';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import testCenters from '../data/testCenters';
-const drawerMenuItemData = [];
+//const drawerMenuItemData = [];
 
-export default function stateCenterMenu() {
+export default function stateCenterMenu(props) {
   const [selectedIndex, setSelectedIndex] = React.useState('');
-  const drawerMenuItemData = testCenters;
+  //const drawerMenuItemData = testCenters;
   const handleClick = (index) => {
     if (selectedIndex === index) {
       setSelectedIndex('');
@@ -18,6 +19,23 @@ export default function stateCenterMenu() {
       setSelectedIndex(index);
     }
   };
+  const statelist = [
+    ...new Set(testCenters.map((locations) => locations.state)),
+  ];
+  let drawerMenuItemData = [];
+  statelist.forEach((place) => {
+    let cities = [];
+    testCenters.forEach((obj) => {
+      // console.log(obj);
+      //console.log(place);
+      if (obj.state === place) cities.push(obj.city);
+    });
+    let statecityindex = {
+      state: place,
+      cities: cities,
+    };
+    drawerMenuItemData.push(statecityindex);
+  });
   return (
     <List
       component="nav"
@@ -38,15 +56,12 @@ export default function stateCenterMenu() {
                 handleClick(index);
               }}
             >
-              <ListItemIcon>
-                <item.icon />
-              </ListItemIcon>
               <ListItemText primary={item.state} />
               {index === selectedIndex ? <ExpandLess /> : <ExpandMore />}
             </ListItem>
             <Collapse in={index === selectedIndex} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
-                {item.submenu.map((sub, index) => {
+                {item.cities.map((sub, index) => {
                   return (
                     <ListItem button>
                       <ListItemText primary={sub.name} />
