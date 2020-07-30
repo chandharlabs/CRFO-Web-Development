a939f97b05f43dbc8e2bd6a12194525bea183156
chadharlabs <prabhu@chandhar-labs.com>
2020-07-31 01:03:45 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.093a1975edc67f3b0d85a545eea075e3.js"
+  "/precache-manifest.792883e9bebedd9a29921fc1909577dc.js"
 );
 
 self.addEventListener('message', (event) => {
