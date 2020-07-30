d3d4bbde139f07fde281148e5edf28bac6245367
chandharlabs <prabhu@chandhar-labs.com>
2020-07-30 14:36:18 +0530

    Updates

---
+++ precache-manifest.ce4d52837aac8ff153d0c1ab21f88175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880b6f13f84e11f7dfc85b2b38757c27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bd0c6aaa0e549837908",
+    "url": "/static/css/main.92bfd8e9.chunk.css"
+  },
+  {
+    "revision": "981eb7990c9f58ddf44e",
+    "url": "/static/js/2.707bd8e8.chunk.js"
+  },
+  {
+    "revision": "bc3b011d10123c3e38762c80cd3383a4",
+    "url": "/static/js/2.707bd8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd0c6aaa0e549837908",
+    "url": "/static/js/main.4c2d0d6b.chunk.js"
+  },
+  {
+    "revision": "41110f5c0d041429caad",
+    "url": "/static/js/runtime-main.0aee60e1.js"
+  }
+]);
