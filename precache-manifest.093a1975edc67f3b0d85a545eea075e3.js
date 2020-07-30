641d38c2e0547295dec04eeb3f3eb813fd5c77c6
chandharlabs <prabhu@chandhar-labs.com>
2020-07-30 18:47:22 +0530

    Updates

---
+++ precache-manifest.093a1975edc67f3b0d85a545eea075e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472438b2aa4cf056e10cec06211b224e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa59c980167dc3f4c47",
+    "url": "/static/css/main.92bfd8e9.chunk.css"
+  },
+  {
+    "revision": "981eb7990c9f58ddf44e",
+    "url": "/static/js/2.707bd8e8.chunk.js"
+  },
+  {
+    "revision": "bc3b011d10123c3e38762c80cd3383a4",
+    "url": "/static/js/2.707bd8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa59c980167dc3f4c47",
+    "url": "/static/js/main.59932cda.chunk.js"
+  },
+  {
+    "revision": "41110f5c0d041429caad",
+    "url": "/static/js/runtime-main.0aee60e1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/data.crfo.org/precache-manifest.2de7c50a91ad98941068b6f967cf05d7.js"
+  "/precache-manifest.093a1975edc67f3b0d85a545eea075e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/data.crfo.org/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
