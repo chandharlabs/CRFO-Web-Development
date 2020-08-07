aee9c3336e4ad09a3bf55392ceb7b3d8580fa4cb
adgai19 <gaitondeaditya@gmail.com>
2020-08-07 13:20:12 +0530

    removed unnecessary dependancies and files

--- src/components/stateWiseList/IndiaData.js
+++ src/components/stateWiseList/IndiaData.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import classNames from 'classnames/bind';
-import StateCenterMenu from '../statelist';
 import testCenter from '../../data/testCenters';
 import Dropdown from '../stateListDropdown';
 
 const cx = classNames.bind(require('./stateWiseList.module.css'));
 
 export default function IndiaData(props) {
-  const { onStateSelect, viewTestCenters, viewTowers } = props;
-
-  // const handleTestCentersToggle = () => {
-  //   props.onTestCenterToggle(!viewTestCenters);
-  // };
-  // const handleTowersToggle = () => {
-  //   props.onTowersToggle(!viewTowers);
-  // };
+  const { onStateSelect } = props;
 
   const handleTestClick = (center) => {
     const selectedState = testCenter.find((location) => {
@@ -29,7 +21,6 @@ export default function IndiaData(props) {
     <>
       <section className={cx('list-wrapper')}>
         <section className={cx('list-content')}>
-          {/* <StateCenterMenu handleStateClick={handleTestClick} /> */}
           <Dropdown
             handleStateClick={handleTestClick}
             handleStateReset={stateReset}
